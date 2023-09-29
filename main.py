8194d805caadc0146b7f35e6cefa88364a163540
Oli Szymanski <oliwier.szymanski112@gmail.com>
2023-09-29 06:06:55 +0200

    initial commit

---
+++ main.py
@@ -0,0 +1,6 @@
+import data.data_vis as data_vis
+
+if (__name__ == '__main__'):
+    data_vis.align_two_seq( type='local', alignment_00="ACCGT", alignment_01="ACG" )
+
+    # data_vis.align_multiple_seq( './data/genomes/', format='fasta' )

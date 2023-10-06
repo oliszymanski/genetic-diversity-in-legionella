0493310d5b047f8064b56ad3caf1fbaca1153690
Oli Szymanski <oliwier.szymanski112@gmail.com>
2023-10-06 16:14:36 +0200

    testing and working

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import data.bio_data_vis as bio_data_vis
 
+from Bio import SeqIO
+
 if ( __name__ == '__main__' ):
     main_file_path = r'./sequences.fna'
-
-    
 
     wanted_ids = [
         'NZ_AP022841.1',        # Legionella antarctica
@@ -14,7 +14,9 @@ if ( __name__ == '__main__' ):
         'NZ_LT906442.1'         # Legionella waltersii
     ]
     
-    # bio_data_vis.write_sequences_to_align( main_file_path, wanted_ids )
-    aligned_sequences = bio_data_vis.align_multiple_seq_from_file( './retrived_seqs.fna' )
+    # aligned_sequences = bio_data_vis.align_multiple_seq_from_file( './retrived_seqs.fna' )
 
-    # bio_data_vis.align_two_seq( 'global', ls_record_penumophila[0], ls_record_antarctica[0] )
+    retrived_nucleotides = bio_data_vis.retrive_first_nucleotides( file_path=r'./retrived_seqs.fna', n_nucleotides=12000 )
+    bio_data_vis.write_fasta_file( retrived_nucleotides, r'to_align.fna' )
+
+    aligned_sequences = bio_data_vis.align_multiple_seq_from_file( r'./to_align.fna' )
